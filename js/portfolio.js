6f463d5b484e15dcc14d0e5b99b2245ee7dc7bba
Tina <mj.tinashin@gmail.com>
2015-11-10 02:52:11 -0600

    individual stock clickable and links to own page

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,10 @@
 $('#myTabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
-})
+});
+
+$(document).ready(function($) {
+	$(".clickable").click(function() {
+		window.document.location = $(this).data("href");
+	});
+});
